99ec5ce95a4d03c59bb32da3d14b2975c99afa30
Erzin473 <erzin.etem@abv.bg>
2023-01-02 19:12:42 +0200

    Custom Logger

---
+++ MagicVilla_VillaAPI/Logging/Logging.cs
@@ -0,0 +1,19 @@
+﻿using MagicVilla_VillaAPI.Loging;
+
+namespace MagicVilla_VillaAPI.Logging
+{
+    public class Logging : ILogging
+    {
+        public void Log(string message, string type)
+        {
+            if (type == "error") 
+            {
+                Console.WriteLine("ERORR - " + message);
+            }
+            else
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
